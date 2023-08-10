e47785d36118f9da9c61f4ce9ffef40de90c0ef8
M-BEDH <mel.b.ms@pm.me>
2023-08-10 09:46:49 +0200

    Responsive 30 lettres

--- src/composants/Todo.jsx
+++ src/composants/Todo.jsx
@@ -79,7 +79,7 @@ function TodoList() {
         type="textarea"
         value={inputValue}
         onChange={handleInputChange}
-        maxLength={26}
+        maxLength={30}
         placeholder="Quelle est votre tâche ?"
       />
 
